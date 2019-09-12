8cefa611c7e8f265244962be42b2ddc88e6548ad
Luikson Silva <luikson97@hotmail.com>
2019-09-12 12:01:50 -0300

    Update flowWhen.kt

--- Kotlin/PrimeiroProjeto/src/flowWhen.kt
+++ Kotlin/PrimeiroProjeto/src/flowWhen.kt
@@ -29,6 +29,28 @@ fun verificaMaior(a: Int, b:Int){
     else
         println("$b é maior")
 }
+
+fun calculaValorAbacaxi(qtd: Int) : Int{
+    //qtd < 10, cada abacaxi custa 3
+    //qtd > 10, cada abacaxi custa 2
+    when{
+        qtd in 0..9 -> return qtd*3
+
+        qtd >= 10 -> return qtd*2
+
+        else -> return 0
+    }
+}
+
+fun verificaCategoria(peso: Float){
+    when{
+        peso < 57.00 -> println("Pena")
+        peso in 57.00..60.99 -> println("Leve")
+        peso in 61.00..72.99 -> println("Médio")
+        else -> println("Pesado")
+    }
+}
+
 fun main(){
     val op1 = operacao(10,10,"Soma")
     val op2 = operacao(10,10,"Subtracao")
@@ -41,4 +63,11 @@ fun main(){
     verificaMaior(5,3)
     verificaMaior(5,5)
     verificaMaior(3,5)
+    println(calculaValorAbacaxi(9))
+    println(calculaValorAbacaxi(10))
+    println(calculaValorAbacaxi(15))
+    verificaCategoria(50.0f)
+    verificaCategoria(60.0f)
+    verificaCategoria(70.0f)
+    verificaCategoria(80.0f)
 }
