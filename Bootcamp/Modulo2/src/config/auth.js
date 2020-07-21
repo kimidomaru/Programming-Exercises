4157c64fd786972025ecdd36188bb4731bac704a
Luikson <luikson97@hotmail.com>
2020-07-21 15:50:57 -0300

    Modulo2 updated

---
+++ Bootcamp/Modulo2/src/config/auth.js
@@ -0,0 +1,4 @@
+export default{
+    secret: 'a4aa9756049fcfa828e899606ff89023',
+    expiresIn: '7d',
+};
--- Bootcamp/Modulo2/src/routes.js
+++ Bootcamp/Modulo2/src/routes.js
@@ -1,12 +1,27 @@
 import { Router } from 'express';
 import User from './app/models/User';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMidleware from './app/midlewares/auth';
 
 //const { Router } = require('express');
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMidleware);
+routes.put('/users', UserController.update);
+
+//routes.post('/users', UserController.store);
+
+/*routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+
+routes.put('/users', UserController.update);*/
 
 /*routes.get('/', async(req, res) => {
     const user = await User.create({
